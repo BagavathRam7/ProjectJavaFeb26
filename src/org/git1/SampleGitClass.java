71c22da12d900121bb1f926558a64602b62351e8
BagavathRam7 <bagavathram7@gmail.com>
2026-02-23 13:59:17 +0530

    done

---
+++ src/org/git1/SampleGitClass.java
@@ -0,0 +1,23 @@
+package org.git1;
+
+public class SampleGitClass {
+	
+public static void gitData1() {
+		
+		System.out.println("GitData 1");
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		gitData1();
+		
+		int a1=4443;
+		System.out.println(a1);
+		System.out.println("Branch A start the work");
+		String data = "Test Data Verified";
+		System.out.println(data);
+	}
+	
+
+}
