31aabd8adf44df0547524bfdc61ce07ba0990cf3
BagavathRam7 <bagavathram7@gmail.com>
2026-02-23 15:52:58 +0530

    ram work

--- src/org/git1/SampleGitClass.java
+++ src/org/git1/SampleGitClass.java
@@ -14,10 +14,20 @@ public static void gitData1() {
 		
 		int a1=4443;
 		System.out.println(a1);
-		System.out.println("Branch A start the work");
-		String data = "Test Data Verified";
-		System.out.println(data);
-	}
-	
+		
+		long b = 7888777;
+		
+		if(b==565) {
+			System.out.println("Condition True");
+		}
+		
+		else {
+			
+			System.out.println("Condition False");
+		}
+		
+		System.out.println("Ram work done");
 
+	
+	}
 }
